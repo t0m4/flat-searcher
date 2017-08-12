b6d5a5258e3f6918fe7bbc4f9dedfdecc12b58aa
Tamas Sandor <tamas.sandor@planorama.com>
2017-08-12 18:56:28 +0200

    fix

--- services/fbmessage.js
+++ services/fbmessage.js
@@ -6,8 +6,12 @@ let fbApi;
 
 function login() {
   return new Promise((resolve, reject) => {
-    if (!process.env.FB_EMAIL || !process.env.FB_PASSWORD) return resolve();
-    return getApi({ email: process.env.FB_EMAIL, password: process.env.FB_PASSWORD }, (err, api) => {
+    if ((!process.env.FB_EMAIL || !process.env.FB_PASSWORD) && !process.env.APP_STATE) return resolve();
+    let credentials = {};
+    if (process.env.APP_STATE) credentials = { appState: process.env.APP_STATE };
+    else credentials = { email: process.env.FB_EMAIL, password: process.env.FB_PASSWORD };
+
+    return getApi(credentials, (err, api) => {
       if (err) return reject(err);
       if (process.env.FB_SENDER_ID) api.setOptions({ pageID: process.env.FB_SENDER_ID });
       fbApi = api;
